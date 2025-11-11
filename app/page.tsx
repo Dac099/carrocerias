43c0da4d3c29b2bf4e75d20f4e3b2d8c0e6c35f6
David Ceja Zapata <d.ceja@ynemx.com>
2025-11-11 09:37:14 -0600

    define layout for the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <article className="w-full h-full grid md:grid-cols-2 md:grid-rows-2 gap-2 p-1 grid-cols-1 grid-rows-3">
+      <section className="border border-dashed border-indigo-400 rounded-lg content-center">
+        <p className='text-center'>Últimas cotizaciones</p>
+      </section>
+      <section className="border border-dashed border-indigo-400 rounded-lg content-center">
+        <p className='text-center'>Trabajos empezados</p>
+      </section>
+      <section className="border border-dashed border-indigo-400 md:col-span-2 rounded-lg content-center">
+        <p className='text-center'>Métricas</p>
+      </section>
+    </article>
   );
 }
